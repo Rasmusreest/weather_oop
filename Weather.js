78fc0c3044cd7ae5d77c0181a803bc7e83b3463e
Rasmus <rasmus.reest@khk.ee>
2020-12-15 14:14:31 +0200

    Weather data is displayed

--- Weather.js
+++ Weather.js
@@ -1,13 +1,13 @@
 class Weather {
     constructor(city) {
         this.city = city;
-        this.key = "1fbbb7f9023e148d00df6a5cf216e20d";
+        this.key = '1fbbb7f9023e148d00df6a5cf216e20d';
     }
 
     // data from API
     async weatherData() {
         const resp = await fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=Toronto,CA&appid=" +
+            "https://api.openweathermap.org/data/2.5/weather?q=Tartu,EE&appid=1fbbb7f9023e148d00df6a5cf216e20d" +
             this.city +
             "&appid=" +
             this.key
