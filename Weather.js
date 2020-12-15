8ea963fefe96e5e67cf4b3284f848a4df3b39414
Rasmus <rasmus.reest@khk.ee>
2020-12-15 15:12:12 +0200

    Default city weather shown

--- Weather.js
+++ Weather.js
@@ -5,8 +5,8 @@ class Weather {
     }
 
     // data from API
-    async weatherData() {
-        const resp = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Tartu,EE&appid=021259759086656f1609cbf58a2902a7' +
+    async weatherData(){
+        const resp = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' +
             this.city + '&appid=' + this.key);
         const data = await resp.json();
         return data;
